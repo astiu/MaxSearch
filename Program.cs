dbcec1aec9f5f4b3635446ede2b17aa5297d33b1
astiu <72610218+astiu@users.noreply.github.com>
2020-10-26 20:45:15 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace VS_code
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 5;
+            Random a = new Random();
+            int max = 0;
+            
+           int [] b = new int [N];
+           for(int i=0; i<N; i++)
+           {
+                b[i]= a.Next(-10,10);
+             Console.WriteLine(b[i]); 
+           }
+
+            max = b[0];
+            for(int i =0; i<N; i++)
+            {
+                
+                if(b[i]>max)
+                {
+                    max=b[i];
+                }
+            }
+         Console.WriteLine(max);
+          
+            Console.ReadKey();
+
+        }
+    }
+}
